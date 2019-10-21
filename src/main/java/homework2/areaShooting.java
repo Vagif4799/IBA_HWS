ad090b201c4a0ab62542a65705fac26a0a19a411
Vagif <quliyev_vaqif@icloud.com>
2019-10-21 14:32:00 +0400

    -- Hw 2 and Hw 3 ready almost

--- src/main/java/homework2/areaShooting.java
+++ src/main/java/homework2/areaShooting.java
@@ -13,24 +13,64 @@ import java.util.*;
 public class areaShooting {
     public static void main(String[] args) {
 
-     int[][] arr = new int[6][6];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("All set. Get ready to rumble!");
+
+        System.out.println("How is your name? ");
+        String name;
+        name = scan.nextLine();
+
+        System.out.println();
 
 
-        for(int i=0; i<arr.length; i++) {
-            for (int j=0; j<arr.length; j++) {
-                arr[i][j] = j;
+        String[][] arr = new String [5][5];
+
+        for (int i=0; i<arr.length; i++){
+            for (int j=0; j<arr.length; j++){
+                arr[i][j] = "-";
             }
         }
 
-        for(int i=0; i<arr.length; i++) {
-            for (int j=0; j<arr.length; j++) {
-                System.out.print(arr[i][j]);
+        for (int i=0; i<arr.length; i++){
+            for (int j=0; j<arr.length; j++){
+                System.out.print(arr[i][j] + " |");
             }
             System.out.println();
         }
 
 
+        int randomRow = (int)(Math.random()*5);
+        int randomColumn = (int)(Math.random()*5);
+
+        int userRow;
+        int userColumn;
+
+
+        while (true){
+            System.out.print("Please, enter a line for fire: ");
+            userRow = scan.nextInt();
+            System.out.print("Please, enter a shooting bar: ");
+            userColumn = scan.nextInt();
+
+            if(randomRow == userRow && randomColumn == userColumn) {
+                arr[userRow-1][userColumn-1] = "x";
+                System.out.println("Congrats " + name);
+                break;
+            } else if (randomRow != userRow && randomColumn != userColumn){
+                arr[userRow-1][userColumn-1] = "*";
+            }
+
+            for (int i=0; i<arr.length; i++){
+                for (int j=0; j<arr.length; j++){
+                    System.out.print(arr[i][j] + " |");
+                }
+                System.out.println();
+            }
+
+        }
 
     }
 
 }
+
