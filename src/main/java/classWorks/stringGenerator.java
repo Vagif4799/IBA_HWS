57781803ce142748ec24f7d09ef22df5f37166d3
Vagif <quliyev_vaqif@icloud.com>
2019-10-22 09:41:57 +0400

    -- added function

--- src/main/java/classWorks/stringGenerator.java
+++ src/main/java/classWorks/stringGenerator.java
@@ -5,28 +5,28 @@ import java.util.*;
 public class stringGenerator {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        String s1 = randomWord(10);
+        String s2 = randomWord(5);
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
+
+    public static String randomWord (int length) {
 
         int down = 97;
         int up = 122;
-        int givenLength;
-
-        System.out.print("Please, write the desired length of the String you want: ");
-
-        givenLength = scan.nextInt();
 
         Random random = new Random();
         String word = "";
-        for (int i = 0; i < givenLength; i++) {
+        for (int i = 0; i < length; i++) {
             int randomLimitedInt = down + (int)
                     (random.nextFloat() * (up - down + 1));
             word += ((char) randomLimitedInt);
         }
         String result = word.toString();
 
-        System.out.println(word);
-
-
+        return word;
 
     }
 
