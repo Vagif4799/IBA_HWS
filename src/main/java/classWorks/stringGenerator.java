7156d244df18a8c5c96d019036ec504c0200b733
Vagif <quliyev_vaqif@icloud.com>
2019-10-22 09:35:36 +0400

    -- bug fixes

--- src/main/java/classWorks/stringGenerator.java
+++ src/main/java/classWorks/stringGenerator.java
@@ -7,19 +7,19 @@ public class stringGenerator {
 
         Scanner scan = new Scanner(System.in);
 
-        int leftLimit = 97;
-        int rightLimit = 122;
-        int specificLength;
+        int down = 97;
+        int up = 122;
+        int givenLength;
 
         System.out.print("Please, write the desired length of the String you want: ");
 
-        specificLength = scan.nextInt();
+        givenLength = scan.nextInt();
 
         Random random = new Random();
         String word = "";
-        for (int i = 0; i < specificLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+        for (int i = 0; i < givenLength; i++) {
+            int randomLimitedInt = down + (int)
+                    (random.nextFloat() * (up - down + 1));
             word += ((char) randomLimitedInt);
         }
         String result = word.toString();
