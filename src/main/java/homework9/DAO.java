43a610edee4a8ff3b9803717a90921bb54685d0d
Vagif <quliyev_vaqif@icloud.com>
2019-11-10 08:20:38 +0400

    -- hw 9 dao interface added

---
+++ src/main/java/homework9/DAO.java
@@ -0,0 +1,18 @@
+package homework9;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<T> {
+
+    List<T> getAll();
+
+    Optional<T> get(long id);
+
+    boolean deleteById(long id);
+
+    boolean deletebyFamily(T t);
+
+    void save(T t);
+
+}
