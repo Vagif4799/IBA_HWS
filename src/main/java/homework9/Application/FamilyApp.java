995c929e4149f9c9b5b33a00a3f942e306cdd5b0
Vagif <quliyev_vaqif@icloud.com>
2019-11-11 08:58:19 +0400

    -- hw 9 final stages

--- src/main/java/homework9/Application/FamilyApp.java
+++ src/main/java/homework9/Application/FamilyApp.java
@@ -13,51 +13,7 @@ public class FamilyApp {
 
     public static void main(String[] args) {
 
-//
-//        /*
-//        {
-//                {DayOfWeek.MONDAY.name(), "Go"},
-//                {DayOfWeek.TUESDAY.name(), "Read"},
-//                {DayOfWeek.WEDNESDAY.name(), "Walk"},
-//                {DayOfWeek.THURSDAY.name(), "Study"},
-//                {DayOfWeek.FRIDAY.name(), "Listen"},
-//                {DayOfWeek.SATURDAY.name(), "Sleep"},
-//                {DayOfWeek.SUNDAY.name(), "Netflix"}
-//        };
-//        */
-//
-//        Map<String, String> tb = new HashMap<String, String>();
-//
-//
-//        Schedule schedule = new Schedule(tb);
-//        Schedule table = new Schedule(tb);
-//        Human angeline = new Human("Angel", "July", 1974);
-//        Human brad = new Human("Brad", "Pit", 1964, table);
-//        Human jon = new Human("Jon", "Johnny", 2000);
-//        Human kind = new Human("Donald", "Trump", 2005);
-//        String[] habits = {"Barking", "Eating", "Sleeping"};
-//        Dog dog = new Dog(Pet.Species.DOG, "Bool", 70);
-//        Fish fish = new Fish(Pet.Species.FISH, "Dole", 20);
-//        Set<Pet> pets = new HashSet<Pet>();
-//        pets.add(dog);
-//        pets.add(fish);
-//
-//
-//        Family family = new Family();
-//        family.setFather(brad);
-//        family.setMother(angeline);
-//        family.addChild(jon);
-//        family.addChild(kind);
-//        family.setPet(pets);
-//        family.deleteChild(1);
-//
-//
-//        System.out.println();
-//        System.out.print("Number of family members: ");
-//        System.out.println(family.countFamily(family));
-//        System.out.println();
-//        System.out.println("****** About Family Members ******");
-//        System.out.println(family.toString());
+
 
         familyDao = new FamilyDAO();
         FamilyController familyController = new FamilyController();
